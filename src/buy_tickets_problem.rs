2cad811b3d53a3a4d201b41ab64e5f86a901b06b
btooom1999 <btooom1999@gmail.com>
2025-12-17 10:39:52 +0700

    optimize buy_tickets_problem exercise code

--- src/buy_tickets_problem.rs
+++ src/buy_tickets_problem.rs
@@ -4,12 +4,12 @@ pub fn main() {
     let n = 5;
     let t = [2, 5, 7, 8, 4];
     let mut r = [4, 9, 10, 10].to_vec();
-
-    let mut dp = vec![0; n+1];
     r.insert(0, 0);
 
+    let mut dp = vec![0; n+1];
+
     dp[1] = t[0];
-    for i in 2..=r.len() {
+    for i in 2..=n {
         dp[i] = cmp::min(dp[i-1] + t[i-1], dp[i-2] + r[i-1]);
     }
 
