5036b8f7209bfe32ebfb2158c2dace3f4e3aa79e
btooom1999 <btooom1999@gmail.com>
2025-12-17 15:51:16 +0700

    steps_problem

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod longest_increasing_subsequence;
 mod buy_tickets_problem;
 mod maximum_path_sum_in_a_matrix;
 mod domino_tile;
+mod steps_problem;
 
 fn main() {
-   domino_tile::main();
+   steps_problem::main();
 }
---
+++ src/steps_problem.rs
@@ -0,0 +1,22 @@
+pub fn main() {
+    let n = 4;
+    let k = 1; // the number of broken steps
+    let broken_steps = [2];
+
+    let mut dp = vec![0; n+1];
+    dp[1] = 1;
+    
+    // 0 is secure steps
+    // 1 is insecure steps
+    let mut status_of_steps = vec![0;n+1];
+    for i in broken_steps.iter() {
+        status_of_steps[i.to_owned()] = 1;
+    }
+
+    for i in 2..=n {
+        if status_of_steps[i] == 1 { continue; }
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    println!("{}", dp[n]);
+}
