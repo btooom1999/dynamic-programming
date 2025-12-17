4137883d01cdbb1fc5dc788f0b4add35d14d6424
btooom1999 <btooom1999@gmail.com>
2025-12-17 12:52:12 +0700

    finished maximum_path_sum_in_a_matrix exercise

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 mod fibonacci;
 mod longest_increasing_subsequence;
 mod buy_tickets_problem;
+mod maximum_path_sum_in_a_matrix;
 
 fn main() {
-   buy_tickets_problem::main();
+    maximum_path_sum_in_a_matrix::main();
 }
---
+++ src/maximum_path_sum_in_a_matrix.rs
@@ -0,0 +1,56 @@
+use std::cmp;
+
+pub fn main() {
+    let n = 5;
+    let k = 7;
+    let matrix = [
+        [9, -2, 6, 2, 1, 3, 4],
+        [0, -1, 6, 7, 1, 3, 3],
+        [8, -2, 8, 2, 5, 3, 2],
+        [1, -1, 6, 2, 1, 6, 1],
+        [7, 2, 6, 2, 1, 3, 7]
+    ].to_vec();
+
+    let mut results = vec![vec![0;1]; n];
+
+    let mut max = matrix[0][0];
+
+    for i in 0..n {
+        results[i][0] = matrix[i][0];
+    }
+
+    for j in 1..k {
+        for i in 0..n {
+            let prev_val = results[i][j-1];
+
+            let ai = (cmp::max(i.saturating_sub(1), 0), j);
+            if let Some(val) = results.get(ai.0).unwrap().get(ai.1) {
+                results[ai.0][ai.1] = cmp::max(val.to_owned(), prev_val + matrix[ai.0][ai.1]);
+            } else {
+                results[ai.0].push(prev_val + matrix[ai.0][ai.1]);
+            }
+
+            let mi = (i, j);
+            if let Some(val) = results.get(mi.0).unwrap().get(mi.1) {
+                results[mi.0][mi.1] = cmp::max(val.to_owned(), prev_val + matrix[mi.0][mi.1]);
+            } else {
+                results[mi.0].push(prev_val + matrix[mi.0][mi.1]);
+            }
+
+            let bi = (cmp::min(i.saturating_add(1), n - 1), j);
+            if let Some(val) = results.get(bi.0).unwrap().get(bi.1) {
+                results[bi.0][bi.1] = cmp::max(val.to_owned(), prev_val + matrix[bi.0][bi.1]);
+            } else {
+                results[bi.0].push(prev_val + matrix[bi.0][bi.1]);
+            }
+
+            max = cmp::max(max, results[i][j]);
+        }
+    }
+
+    for item in results.iter() {
+        println!("{:?}", item);
+    }
+
+    println!("maximum path sum is: {}", max);
+}
