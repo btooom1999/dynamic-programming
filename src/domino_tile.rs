d6ad8a6ed3f6d3c6fddf2cba6c21b595372a68fb
btooom1999 <btooom1999@gmail.com>
2025-12-17 14:03:17 +0700

    finished domino_tile exercise

---
+++ src/domino_tile.rs
@@ -0,0 +1,13 @@
+pub fn main() {
+    let n = 5;
+    let mut dp = [0; 100];
+
+    dp[1] = 1; // vertical tile 2x1
+    dp[2] = 2; // horizontal tile 2x2
+
+    for i in 3..=n {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    println!("{}", dp[n]);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod fibonacci;
 mod longest_increasing_subsequence;
 mod buy_tickets_problem;
 mod maximum_path_sum_in_a_matrix;
+mod domino_tile;
 
 fn main() {
-    maximum_path_sum_in_a_matrix::main();
+   domino_tile::main();
 }
