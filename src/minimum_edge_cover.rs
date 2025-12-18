bf0faa9dd32b4f39a2e81bbda089ba74ebe49705
btooom1999 <btooom1999@gmail.com>
2025-12-18 14:30:58 +0700

    fix setting wrong index for dp[1]

--- src/minimum_edge_cover.rs
+++ src/minimum_edge_cover.rs
@@ -3,11 +3,11 @@ pub fn main() {
     let mut k = [2, 2, 3, 2, 2].to_vec(); // the distances between i and i+1
 
     let mut dp = vec![i32::MAX; n];
-    dp[1] = k[1];
+    dp[1] = k[0];
 
     for i in 2..n {
         dp[i] = std::cmp::min(dp[i-2], dp[i-1]) + k[i-1];
     }
 
-    println!("{:?}", dp);
+    println!("{}", dp[n-1]);
 }
